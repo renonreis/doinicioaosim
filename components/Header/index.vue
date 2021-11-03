af20f9483d6c2aacc739d574e633d2e58f15906c
aurudin <renon@renon.com.br>
2021-11-02 23:27:33 -0300

    feat: create app

---
+++ components/Header/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col">TESTE</div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style src="./styles.scss" lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Main>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h1>Main</h1>
+          </div>
+        </div>
+      </div>
+    </Main>
+  </div>
+</template>
+
+<script>
+import Main from '@/components/Main'
+export default {
+  components: {
+    Main
+  }
+}
+</script>
