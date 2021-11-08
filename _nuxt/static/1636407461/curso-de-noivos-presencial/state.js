23ba32a0f54b4acbadb0a80de499d635570b7331
renonreis <renonreis@users.noreply.github.com>
2021-11-08 22:22:45 +0000

    deploy: 224f6c9fa4505ab22afafebf53487e8a3872df2b

--- _nuxt/static/1636407461/curso-de-noivos-presencial/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdoinicioaosim\u002F_nuxt\u002Fstatic\u002F1636407461",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcurso-de-noivos-presencial",config:{_app:{basePath:"\u002Fdoinicioaosim\u002F",assetsPath:"\u002Fdoinicioaosim\u002F_nuxt\u002F",cdnURL:a}}}}(null));
