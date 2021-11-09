bdc2a4a009c88499f1db76f9e4bf4086bf75fa83
renonreis <renonreis@users.noreply.github.com>
2021-11-09 02:22:59 +0000

    deploy: caf293c05c48fb3b8a9b826e0f9a54792b61c54a

--- _nuxt/static/1636418294/curso-na-sua-igreja/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/curso-na-sua-igreja", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1636424577/curso-de-noivos-presencial/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/curso-de-noivos-presencial", {data:[{_img:{}}],fetch:{},mutations:void 0});
