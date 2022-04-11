6a75db48d68c670528c8aef65a4cb61587453997
renonreis <renonreis@users.noreply.github.com>
2022-04-11 12:25:12 +0000

    deploy: 201b369f8e696645f0f1aaa139e08a6c2b9b6e9e

--- _nuxt/static/1648929979/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F404","\u002F","\u002Fnoivos\u002Fcurso-de-noivos-em-curitiba","\u002Fcurso-de-noivos","\u002Fsobre","\u002Fcurso-na-sua-igreja","\u002Fsolicitar-subsidio"]})
---
+++ _nuxt/static/1649679900/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fnoivos\u002Fcurso-de-noivos-em-curitiba","\u002F404","\u002Fcurso-de-noivos","\u002Fcurso-na-sua-igreja","\u002Fsobre","\u002Fsolicitar-subsidio"]})
